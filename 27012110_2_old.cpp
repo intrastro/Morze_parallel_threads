280b98fd808796285d512a32d21dafb379e10be6
intrasto <35892883+intrastro@users.noreply.github.com>
2018-01-28 17:25:44 +0300

    Add files via upload

---
+++ 27012110_2_old.cpp
@@ -0,0 +1,14 @@
+#include "morze.h"
+
+int main ()
+    {
+    std::cout << "main began" << std::endl;
+
+    morze Head_morze;
+
+    //Beep (600, 1500);
+
+    Sleep (100000);
+
+    std::cout << "main ended" << std::endl;
+    }
